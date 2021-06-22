253530df56406982efc607f3f7a3558f8bee442d
Carlos Ramos <carlosmarioramos34@gmail.com>
2021-06-21 20:40:28 -0500

    Se cambia el modo de renderizado de los skills: ahora se lee un json con la data y se renderiza con un ciclo.

--- src/controllers/ctrl-router.js
+++ src/controllers/ctrl-router.js
@@ -1,1 +1,4 @@
-exports.render_index = (req, res) => res.render('index', { title: "Hola mundo" })
+exports.render_index = (req, res) => {
+  const skills = require('../data/skills.json')
+  res.render('index', { skills })
+}
