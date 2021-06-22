0a24d8999fc9a7727f05d4cbb231f1dcd61a08ec
Carlos Ramos <carlosmarioramos34@gmail.com>
2021-06-22 18:37:57 -0500

    Se corrige el grid- layout del sitio. Ahora el footer sí queda abajo y el main se extiende lo más posible.

--- src/controllers/ctrl-router.js
+++ src/controllers/ctrl-router.js
@@ -1,4 +1,10 @@
 exports.render_index = (req, res) => {
   const skills = require('../data/skills.json')
   res.render('index', { skills })
+  return
+}
+
+exports.render_contact = (req, res) => {
+  res.render('contact')
+  return
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const router = Router()
 const ctrlRouter = require('../controllers/ctrl-router')
 
 router.get('/', ctrlRouter.render_index)
+router.get('/contacto', ctrlRouter.render_contact)
 
 module.exports = router
