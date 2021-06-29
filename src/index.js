0ab68af745bcd61879d09ee1e2d4259fe80fc918
Carlos Ramos <carlosmarioramos34@gmail.com>
2021-06-29 10:21:15 -0500

    Se corrigen los estilos del botón de envío del formulario de contacto. Ahora tiene los estilos por defecto predefinidos en el template sass.

--- src/index.js
+++ src/index.js
@@ -25,5 +25,5 @@ app.use(require('./routes/'))
 
 // lintening port
 app.listen(app.get('port'), () => {
-  console.log(`Server running on port ${app.get('port')}`)
+  console.log(`Server running on port ${app.get('port')} http://localhost:${app.get('port')}`)
 })
